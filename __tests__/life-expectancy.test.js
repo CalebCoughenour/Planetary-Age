67ea735d73a571c3adcc09427d07e069d13d95e3
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-04-15 14:41:51 -0700

    add test for mercury life expectancy

--- __tests__/life-expectancy.test.js
+++ __tests__/life-expectancy.test.js
@@ -1,13 +1,28 @@
-import { lifeExpectancyCalculator } from '../src/js/life-expectancy.js';
+import { lifeExpectancyCalculator, mercuryLifeExpectCalc } from '../src/js/life-expectancy.js';
+import AgeObject from '../src/js/age-calculator.js';
 
 describe('lifeExpectancyCalculator', () => {
-  test('should calculate how many years are left between life expectancy', () => {
+
+  let newAge;
+
+  beforeEach(() => {
+    newAge = new AgeObject(29, 80);
+  });
+
+  test('should calculate how many years are left between life expectancy for Earth years', () => {
     let lifeLeft = lifeExpectancyCalculator(29, 80);
     expect(lifeLeft).toEqual(51);    
   });
 
   test('should output how many years passed life expectancy a person is if their age is more than expectancy', () => {
     let lifeLeft = lifeExpectancyCalculator(85, 80);
-    expect(lifeLeft).toEqual("You have live 5 years past your expectancy!");
+    expect(lifeLeft).toEqual("You have lived 5 years past your expectancy!");
+  });
+
+  test('should calculate how many years are left between life expectancy for Mercury years', () => {
+    newAge.convertToEarthDays();
+    let mAge = newAge.convertToMercuryAge();
+    let mercuryLifeLeft = mercuryLifeExpectCalc(mAge, 80);
+    expect(mercuryLifeLeft).toEqual(40);
   });
 });
