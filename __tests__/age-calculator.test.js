c1b9004874bcc1b3a976c9e837d0574d4cb662a2
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-04-15 09:26:37 -0700

    add test for age object, switching direction for project and adding object to interact with

--- __tests__/age-calculator.test.js
+++ __tests__/age-calculator.test.js
@@ -1,14 +1,24 @@
-import { convertToDays } from '../src/js/age-calculator.js';
+import { AgeObject, convertToEarthDays } from '../src/js/age-calculator.js';
 
-describe('convertToDays', () => {
+describe('ageObject', () => {
+
+  test('should receive age input and save it in object')
+  let age = new AgeObject(29);
+  expect(age).toEqual(29);
+});
+
+describe('convertToEarthDays', () => {
 
   test('should receive age as input and save as age variable', () => {
-    let age = convertToDays(29);
-    expect(age).toEqual(29);
+    let totalDays = convertToEarthDays(29);
+    let totalAge = totalDays / 365;
+    expect(totalAge).toEqual(29);
   });
 
   test('should multiply age by 365 to get Earth days relating to age', () => {
-    let earthDaysAlive = convertToDays(29);
-    expect(earthDaysAlive).toEqual(10,585);
+    let earthDaysAlive = convertToEarthDays(29);
+    expect(earthDaysAlive).toEqual(10585);
   });
+
+
 });
