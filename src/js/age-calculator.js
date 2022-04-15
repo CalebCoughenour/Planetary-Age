4668510af5579e35780df517c60f97dcf07d14a4
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-04-15 11:58:57 -0700

    add working function to calculate Saturn age

--- src/js/age-calculator.js
+++ src/js/age-calculator.js
@@ -26,5 +26,24 @@ export default class AgeObject {
     let marsAge = Math.round(this.days / 686);
     return marsAge;
   }
+
+  convertToJupiterAge() {
+    let jupiterAge = Math.round(this.days / 4344);
+    return jupiterAge;
+  }
+
+  // convertToSaturnAge() {
+  //   let saturnAge = Math.round(this.days / 10731);
+  //   return saturnAge;
+  // }
+
+  // checkSaturnAge() {
+  //   let saturnAge = age.convertToSaturnAge();
+  //   if (saturnAge < 1) {
+  //     return "You are less than 1 Saturn year old!";
+  //   }
+  // }
+
+
 }
 
