fb4ada41d6fe4a508f2be6614a19fe0908e0fbef
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-04-15 14:19:40 -0700

    add checker/output if age is less than expected years

--- src/js/life-expectancy.js
+++ src/js/life-expectancy.js
@@ -1,4 +1,8 @@
 export const lifeExpectancyCalculator = (age, expectancy) => {
   let lifeLeft = (expectancy - age);
+  if (lifeLeft < 0) {
+    let absLifeLeft =  ("You have lived " + (Math.abs(lifeLeft)) + " years past your expectancy!");
+    return absLifeLeft;
+  }
   return lifeLeft;
 }
