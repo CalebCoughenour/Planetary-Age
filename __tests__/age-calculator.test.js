3b0a383cb0c825b70c64ef29d146aefd63b78495
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-04-15 09:10:28 -0700

    write test for receiving age and multiply by 365 to get total days alive

--- __tests__/age-calculator.test.js
+++ __tests__/age-calculator.test.js
@@ -1,8 +1,14 @@
 import { convertToDays } from '../src/js/age-calculator.js';
 
 describe('convertToDays', () => {
+
   test('should receive age as input and save as age variable', () => {
     let age = convertToDays(29);
     expect(age).toEqual(29);
+  });
+
+  test('should multiply age by 365 to get Earth days relating to age', () => {
+    let earthDaysAlive = convertToDays(29);
+    expect(earthDaysAlive).toEqual(10,585);
   });
 });
