f286f5c32224dc22f0044c6c83470760b7f9798f
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-04-15 17:16:37 -0700

    add Earth age and days to UI with input form

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import { lifeExpectancyCalculator, mercuryLifeExpectCalc, venusLifeExpectCalc, marsLifeExpectCalc, jupiterLifeExpectCalc } from '../src/js/life-expectancy.js';
+// import { lifeExpectancyCalculator, mercuryLifeExpectCalc, venusLifeExpectCalc, marsLifeExpectCalc, jupiterLifeExpectCalc } from '../src/js/life-expectancy.js';
 import AgeObject from '../src/js/age-calculator.js';
 
 $('#user-input').submit(function(e) {
@@ -11,5 +11,5 @@ $('#user-input').submit(function(e) {
   const userLifeExpec = $('#users-expec').val();
   let userInput = new AgeObject(userAge, userLifeExpec);
   userInput.convertToEarthDays();
-  $('#earth').text("You're Earth age is " + userInput.age + " which is " + userInput.days);
+  $('#earth').text("You're Earth age is " + userInput.age + " which is " + userInput.days + " days!");
 });
